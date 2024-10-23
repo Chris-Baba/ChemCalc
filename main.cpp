fb3240c90f1a37cad9163bcf6a8acef8f2239f18
chris <cbaba001@comcast.net>
2024-10-23 07:17:19 -0400

    Added --verbose option.
    Signed-off-by: chris <cbaba001@comcast.net>

--- main.cpp
+++ main.cpp
@@ -8,8 +8,22 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    // FINISH HERE to read commandline options.
-    qDebug() << "In main.cpp, argc = " << argc << "argv = " << QString(argv[0]);
+    //--- Read commandline options.
+    bool verbose = false;  // Defaults to no qDebug output.
+    QCoreApplication::setApplicationName("ChemCalc");
+    QCommandLineParser parser;
+    parser.setApplicationDescription("A Chemical Calculator");
+    parser.addHelpOption();
+    QCommandLineOption verboseOption(QStringList() << "v" << "verbose", "Verbose debugging output");
+    parser.addOption(verboseOption);
+    // Process the actual command line arguments given by the user
+    parser.process(app);
+    verbose = parser.isSet(verboseOption);
+    if (!verbose){
+        // This turns off qDebug messages in all chemcalc code.
+        QLoggingCategory::setFilterRules("*.debug=false");
+    };
+    //----
 
     MainWindow w;
     w.show();
